3843885ee442640617765177a9dea0b59ebed86c
lmjing69 <lamjingbakhoirom2018@gmail.com>
2026-02-10 00:29:19 +0530

    Change navbar from sticky to relative - no longer overlays content

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -25,7 +25,7 @@ export default function Navbar() {
         }`;
 
     return (
-        <nav className="sticky top-4 z-50 mx-4 sm:mx-6 lg:mx-8">
+        <nav className="relative pt-4 z-50 mx-4 sm:mx-6 lg:mx-8">
             <div className="navbar-glass max-w-7xl mx-auto px-4 sm:px-6 py-4 flex justify-between items-center">
                 {/* LOGO */}
                 <Link
